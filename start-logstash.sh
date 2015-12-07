2e4a8f27761c6cdba0025c20676a47b819b29123
Neale Swinnerton <neale@sw1nn.com>
2015-12-07 18:58:28 +0000

    Use correct ES port.

--- start-logstash.sh
+++ start-logstash.sh
@@ -2,14 +2,14 @@
 
 CONFIG_FILE=/logstash/logstash.conf
 
-if [ -z "${ES_PORT_9300_TCP_ADDR}" -o -z "${ES_PORT_9300_TCP_PORT}" ]; then
+if [ -z "${ES_PORT_9200_TCP_ADDR}" -o -z "${ES_PORT_9200_TCP_PORT}" ]; then
     echo "Must link elasticsearch as es"
     exit 1
 fi
 
 sed -i \
-    -e "s@{{es_hostname}}@${ES_PORT_9300_TCP_ADDR}@" \
-    -e "s@{{es_port}}@${ES_PORT_9300_TCP_PORT}@" \
+    -e "s@{{es_hostname}}@${ES_PORT_9200_TCP_ADDR}@" \
+    -e "s@{{es_port}}@${ES_PORT_9200_TCP_PORT}@" \
     ${CONFIG_FILE}
 
 /logstash/bin/logstash agent \
